aa6147d80f8204071c5328aa8722acb2699243fd
Sean McDonald <Seanmichaelmcdonald@gmail.com>
2020-06-15 15:11:24 -0400

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,11 @@ document.addEventListener("DOMContentLoaded", function() {
   
   var note_el = document.getElementById("the_note");
 
-  note_el.innerHTML = piano_notes[getRandom()].key;
+  note_el.innerHTML = piano_notes[getRandom()].key.toUpperCase();
+  
+  var res = piano_notes[getRandom()].key.replace("s", "#");
+
+  note_el.innerHTML = res;
   
 });
 
