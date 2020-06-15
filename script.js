6624d7409224e32785599e2091c601c270c1ce85
Sean McDonald <Seanmichaelmcdonald@gmail.com>
2020-06-15 15:03:49 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+
+/* const piano_notes*/
+
+function getRandom() {
+  return Math.floor(Math.random() * 88);
+}
+
+const note_el = document.getElementById("the_note");
+
+note_el.innerHTML = piano_notes[getRandom()].note;
+
+
+
+
