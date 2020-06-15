6ebdf4e3b278a8cc371a770ecdde8f7a064970b2
Sean McDonald <Seanmichaelmcdonald@gmail.com>
2020-06-15 15:34:50 -0400

    Update script.js

--- script.js
+++ script.js
@@ -7,21 +7,23 @@ function getRandom() {
 
 document.addEventListener("DOMContentLoaded", function() {
   
-  var note_el = document.getElementById("the_note");
-
-  var rand = getRandom();
-  
-  str = piano_notes[rand].key;
-  var res = str.replace("s", "#");
-
-  note_el.innerHTML = res.toUpperCase();
-  
   var button = document.getElementById("start-button");
-  
   button.addEventListener("click", function(e){
   
+    var note_el = document.getElementById("the_note");
+    var rand = getRandom();
+
+    str = piano_notes[rand].key;
+    var res = str.replace("s", "#");
+    
     var audio = new Audio('GreatAndSoftPiano/'+piano_notes[rand].file+'.mp3');
     audio.play();  
+    
+    setTimeout(function(){ 
+      
+      note_el.innerHTML = res.toUpperCase();
+      
+    }, 2000);
   
   });  
     
