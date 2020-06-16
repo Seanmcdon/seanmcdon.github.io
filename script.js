f6da34c481c22d2b2bd69c3fd9805dd418f6e709
Sean McDonald <Seanmichaelmcdonald@gmail.com>
2020-06-16 10:12:50 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,29 @@ function getRandom() {
   return Math.floor(Math.random() * 88);
 }
 
+var synth = window.speechSynthesis;
+
+var voices = [];
+function populateVoiceList() {
+    voices = synth.getVoices().sort(function (a, b) {
+                                    const aname = a.name.toUpperCase(), bname = b.name.toUpperCase();
+                                    if ( aname < bname ) return -1;
+                                    else if ( aname == bname ) return 0;
+                                    else return +1;
+                                    });
+}
+populateVoiceList();
+
+function speak(text){
+
+        var utterThis = new SpeechSynthesisUtterance(text);
+        
+        utterThis.voice = voices[74];  
+        utterThis.pitch = 1;
+        utterThis.rate = 1;
+        synth.speak(utterThis);
+}
+
 document.addEventListener("DOMContentLoaded", function() {
   
   var button = document.getElementById("start-button");
@@ -22,7 +45,9 @@ document.addEventListener("DOMContentLoaded", function() {
     audio.play().then(function(){  
     
         setTimeout(function(){ 
-
+ 
+          speak(res.toUpperCase());
+                  
           note_el.innerHTML = res.toUpperCase();
 
         }, 2000);
