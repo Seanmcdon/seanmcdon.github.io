8b1724070bfd1ae3b7228c2a80b5a1879ac6767c
Sean McDonald <Seanmichaelmcdonald@gmail.com>
2020-06-16 14:04:03 -0400

    Update script.js

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ function speak(text){
 
 document.addEventListener("DOMContentLoaded", function() {
 	
-document.getElementById("ver").innerHTML = "version 1:59";
+document.getElementById("ver").innerHTML = "version 2:03";
 
                           
   var button = document.getElementById("start-button");
